a371a77984b4a5aee618b2dbfe6ad8070335d280
Willian Garcia <webwil2012@gmail.com>
2024-02-13 18:18:19 -0300

    Desenhando Linha central

--- js/script.js
+++ js/script.js
@@ -2,6 +2,9 @@
 const canvasEl = document.querySelector("canvas"), 
       canvasCtx = canvasEl.getContext("2d");
 
+// definindo variaveis
+let linhaBranca = 15;
+
 // função que inicia o canvas
 function setup(params) {
     // capturando as dimenções da tela
@@ -15,8 +18,17 @@ function setup(params) {
 // função que desenha os elementos
 function draw(params) {
     
+    //desenhando campo
     canvasCtx.fillStyle = "#286047";
     canvasCtx.fillRect(0 , 0 , window.innerWidth, window.innerHeight);
+
+    // desenhando linha central do campo
+    let linhaCentralX = window.innerWidth / 2 - linhaBranca / 2;
+    let linhaCentralY = 0;
+    let linhaCentralLargura = linhaBranca;
+    let linhaCentralAltura = window.innerHeight;
+    canvasCtx.fillStyle = "#FFFFFF";
+    canvasCtx.fillRect(linhaCentralX, linhaCentralY, linhaCentralLargura, linhaCentralAltura);
 
 }
 
