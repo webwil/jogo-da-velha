de2044d49df31a0d4375333aff383f8f42ba715b
Willian Garcia <webwil2012@gmail.com>
2024-02-13 17:05:05 -0300

    iniciando codigos js

---
+++ js/script.js
@@ -0,0 +1,19 @@
+// definindo constantes
+const canvasEl = document.querySelector("canvas"), 
+      canvasCtx = canvasEl.getContext("2d");
+
+// função que inicia o canvas
+function setup(params) {
+    // capturando as dimenções da tela
+    canvasEl.width = window.innerWidth;
+    canvasEl.height = window.innerHeight;
+    canvasCtx.width = window.innerWidth;
+    canvasCtx.height = window.innerHeight;
+    
+}
+
+// função que desenha os elementos
+function draw(params) {
+    
+    setup();
+}
